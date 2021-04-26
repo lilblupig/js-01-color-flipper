1eef2dc237b6d4f3e7c411bc62ec2311672173d6
lilblupig <lilblupig@gmail.com>
2021-04-26 20:50:57 +0100

    Add counter colors.

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -15,6 +15,16 @@ btns.forEach(function(btn) {
     } else {
       count = 0;
     }
+
+    if(count > 0) {
+      value.style.color = "green";
+    }
+    if(count < 0) {
+      value.style.color = "red";
+    }
+    if(count === 0) {
+      value.style.color = "#000";
+    }
     value.textContent = count;
   })
 });
